0510f5e966c97128ae3cf7a0ed4d157f90b046fa
XiuTang <tangxiuemail@qq.com>
2023-08-31 00:45:00 +0800

    Update detected_bug.py

--- submit/detection/detected_bug.py
+++ submit/detection/detected_bug.py
@@ -3,26 +3,23 @@ import random
 class DetectedBug:
     def __init__(self, user_id, bug_id, operator ,original_query, \
                  original_execution_plan,original_ground_truth,original_query_result,\
-                 sqlfile,table_scan): #这里是将后端的信息提取存入对象
+                 sqlfile,table_scan): 
         self.user_id = user_id
         self.bug_id = bug_id
         self.times = 0
         self.is_exit = False
-        self.severity = random.randint(1,3)#随机选择严重程度
-        self.category = "Optimizer" #写死
+        self.severity = random.randint(1,3)
+        self.category = "Optimizer"
         self.operator = operator
-        self.database = "MySQL8.0.28"#写死
+        self.database = "MySQL8.0.28"
         self.original_query = original_query
-        self.minimal_query = "" #不要了
+        self.minimal_query = "" 
         self.original_execution_plan = original_execution_plan
-        self.minimal_execution_plan = ""          #不要了
-        self.minimal_ground_truth = "t1.c0"       #不要了
-        self.minimal_query_result = "t1.c0/1/2/3" #不要了
         self.original_ground_truth = original_ground_truth
         self.original_query_result = original_query_result
         self.sqlfile = sqlfile
         self.table_scan = table_scan
-    def save(self): #这里将上面存入对象的信息存入数据库中
+    def save(self): 
         models.LogicalBug.objects.create(
             user_id=self.user_id,
             bug_id=self.bug_id,
